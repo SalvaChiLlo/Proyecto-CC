8e1bcc718e931bd9a40df95d9a21789e69c9dcc6
Ernesto <ernestogaspar2000@gmail.com>
2022-12-25 17:48:39 +0100

    Añadir tiempo de ejecución

--- worker/src/service/jobLauncher.ts
+++ worker/src/service/jobLauncher.ts
@@ -1,12 +1,20 @@
+import { performance } from "perf_hooks";
 import { Job } from "../models/jobModel";
 import { updateJobStatus } from "../utils/kafka";
 import executeJob from "./jobExecutor";
 
-export async function launchJob(job: Job) {
+export async function launchJob(job: Job) {  
   console.log(job);
   console.log("Lanzando Trabajo");
-
+  
+  const startTime = performance.now()
+  
   const status = await executeJob(job);
+  
+  const endTime = performance.now()
+  const elapsedTime = (endTime - startTime) / 1000;
+
+  status.elapsedTime = elapsedTime;
 
   // SEND RESULTS
   console.log("Trabajo Terminado")
